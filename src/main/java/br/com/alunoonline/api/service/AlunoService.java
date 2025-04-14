973a474f8e2d548986709b7ddaed8bde50bc83c0
ruamhenrique <henriqueruam.rh@gmail.com>
2025-04-14 19:26:27 -0300

    atualizado

--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -5,6 +5,9 @@ import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class AlunoService {
 
@@ -12,6 +15,22 @@ public class AlunoService {
     AlunoRepository alunoRepository;
 
     public void criarAluno(Aluno aluno) {
+
         alunoRepository.save(aluno);
+
     }
+
+    public List<Aluno> listarTodosAlunos() {
+        return alunoRepository.findAll();
+
+    }
+
+    public Optional<Aluno> buscarAlunoPorId(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    public void deletarAlunoPorId(Long id) {
+        alunoRepository.deleteById(id);
+    }
+
 }
